67e9477412dbc614a9a8e4ffc73101944c594a04
Abdul Raheem Abid <abdulraheem.abid@gmail.com>
2021-04-11 17:33:51 +0500

    Adding Http verbs enum

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './interfaces/IApiResponseWrapper';
 export * from './utils/http/custom-exception';
+export * from './utils/http/verbs';
 export * from './utils/common';
 export * from './validators/definition-array-field-type.validator';
 export * from './validators/entry.validator';
