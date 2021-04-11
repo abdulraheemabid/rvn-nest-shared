f51bbdc70e33774982dbbb49c4aa7ca9d7206a71
Abdul Raheem Abid <abdulraheem.abid@gmail.com>
2021-04-12 00:51:51 +0500

    removing validators

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,5 @@ export * from './interfaces/IApiResponseWrapper';
 export * from './utils/http/custom-exception';
 export * from './utils/http/verbs';
 export * from './utils/common';
-export * from './validators/definition-array-field-type.validator';
-export * from './validators/entry.validator';
-export * from './validators/object-type-validator';
 export * from './enums';
 export * from './contracts/das.contract'
