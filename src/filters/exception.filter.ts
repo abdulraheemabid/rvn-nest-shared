fe8e655e990fb3826136d7663a037d7816211ab2
Abdul Raheem Abid <abdulraheem.abid@gmail.com>
2021-04-16 00:17:56 +0500

    adding interceptors and filters

---
+++ src/filters/exception.filter.ts
@@ -0,0 +1,38 @@
+import { ArgumentsHost, Catch, ExceptionFilter, Logger } from '@nestjs/common';
+import { TcpContext } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+import { IApiResponseWrapper } from '../common.interface';
+import { HttpExceptionCustomMessages } from '../http.utils';
+
+export class UnhandledExceptionFilter implements ExceptionFilter {
+  private logger = new Logger(UnhandledExceptionFilter.name);
+
+  catch(exception: any, host: ArgumentsHost) {
+
+    let message = exception?.error?.message || exception?.response?.message || exception?.message || HttpExceptionCustomMessages[exception?.response?.statusCode] || "Request unsuccessfull";
+    const statusCode = exception?.error?.statusCode || exception?.statusCode || exception?.response?.statusCode || 500;
+
+    const pattern = host
+      .switchToRpc()
+      .getContext<TcpContext>()
+      .getPattern();
+
+    let args = null;
+
+    try {
+      args = JSON.stringify(host.switchToRpc().getData());
+    } catch (error) { }
+
+
+    const response: IApiResponseWrapper = {
+      status: "failure",
+      data: null,
+      message,
+      statusCode
+    };
+
+    this.logger.error(`failed: outgoing for pattern: ${pattern} | args: ${args}`);
+
+    return new Observable(sub => { throw response; });
+  }
+}
---
+++ src/interceptors/logging.interceptor.ts
@@ -0,0 +1,30 @@
+import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from '@nestjs/common';
+import { TcpContext } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+export class LoggingInterceptor implements NestInterceptor {
+  private logger = new Logger(LoggingInterceptor.name);
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const pattern = context
+      .switchToRpc()
+      .getContext<TcpContext>()
+      .getPattern();
+
+    let args = null;
+
+    try {
+      args = JSON.stringify(context.switchToRpc().getData());
+    } catch (error) { }
+
+    const now = Date.now();
+
+    this.logger.log(`incoming for pattern: ${pattern} | args: ${args}`);
+      
+    return next.handle().pipe(
+      tap(() => {
+        this.logger.log(`outgoing for pattern: ${pattern} | time: ${Date.now() - now}ms`);
+      })
+    );
+  }
+}
